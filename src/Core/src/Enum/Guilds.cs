3a5070ab63ac469d27ed55bb052cfea752638e14
Chris Tristan <chris.tristan@pm.me>
2022-04-04 18:31:33 -0700

    Initial commit

---
+++ src/Core/src/Enum/Guilds.cs
@@ -0,0 +1,16 @@
+namespace CityOfGuilds.Legion.Core.Enum;
+
+public enum Guilds
+{
+    None = 0,
+    Senate = 1,
+    House = 2,
+    Cult = 3,
+    Clan = 4,
+    Conclave = 5,
+    Syndicate = 6,
+    Swarm = 7,
+    League = 8,
+    Legion = 9,
+    Combine = 10
+}
