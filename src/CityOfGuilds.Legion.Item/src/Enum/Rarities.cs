a8f6d92f1fd5aaf51cab3556933c54cfd70002ab
Chris Tristan <chris.tristan@pm.me>
2022-04-06 18:24:46 -0700

    Added some initial enums.

---
+++ src/CityOfGuilds.Legion.Item/src/Enum/Rarities.cs
@@ -0,0 +1,14 @@
+namespace CityOfGuilds.Legion.Core.Enum;
+
+public enum Rarities
+{
+    Basic = 0,
+    Unique = 1,
+    Common = 2,
+    Uncommon = 3,
+    Rare = 4,
+    Exotic = 5,
+    Epic = 6,
+    Legendary = 7,
+    Mythic = 8
+}
---
+++ src/CityOfGuilds.Legion.Player/src/Enum/GuildPassiveTraits.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CityOfGuilds.Legion.Player.Enum
+{
+    public enum GuildPassiveTraits
+    {
+        None = 0,
+        
+        /// <summary>
+        /// Senate trait. Gives the player the teleport ability.
+        /// </summary>
+        Teleportation = 1,
+
+        /// <summary>
+        /// TODO: House trait.
+        /// </summary>
+        Flash = 2,
+
+        /// <summary>
+        /// TODO: Cult trait. 
+        /// </summary>
+        Menace = 3,
+
+        /// <summary>
+        /// Clan trait. Running turns the player's melee attack into a shoulder charge that does exponentially-more damage and knocks the enemy back, stunning them.
+        /// </summary>
+        Haste = 4,
+
+        /// <summary>
+        /// Conclave trait. Healing reloads ammo.
+        /// </summary>
+        Vigilance = 5,
+
+        /// <summary>
+        /// Syndicate trait. Dealing damage heals the player for a percentage of the damage dealt.
+        /// </summary>
+        Lifelink = 6,
+
+        /// <summary>
+        /// League trait. Using an ability increases all damage dealt for a short time.
+        /// </summary>
+        Prowess = 7,
+
+        /// <summary>
+        /// Swarm trait. Precision shots deal exponentially more damage for every percent less the enemy's health is from 20 percent.
+        /// </summary>
+        Deathtouch = 8,
+
+        /// <summary>
+        /// TODO: Legion trait.
+        /// </summary>
+        DoubleStrike = 9,
+
+        /// <summary>
+        /// Combine trait. Debuffs affect the player for less time. Immune to debuffs after a certain level difference.
+        /// </summary>
+        Hexproof = 10
+    }
+}
